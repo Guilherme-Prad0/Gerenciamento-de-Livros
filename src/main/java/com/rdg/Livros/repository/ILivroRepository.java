6d579f974d1e7bb650d3fa2ef22a5ddae8127203
Augxusto <rodrigo.silva.110805@gmail.com>
2026-03-18 20:28:13 -0300

    service add

--- src/main/java/com/rdg/Livros/repository/ILivroRepository.java
+++ src/main/java/com/rdg/Livros/repository/ILivroRepository.java
@@ -3,6 +3,6 @@ package com.rdg.Livros.repository;
 import com.rdg.Livros.models.LivroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LivroRepository extends JpaRepository<LivroModel, Long> {
+public interface ILivroRepository extends JpaRepository<LivroModel, Long> {
 
 }
---
+++ src/main/java/com/rdg/Livros/service/LivroService.java
@@ -0,0 +1,38 @@
+package com.rdg.Livros.service;
+
+import com.rdg.Livros.models.LivroModel;
+import com.rdg.Livros.repository.ILivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private ILivroRepository livroRepository;
+
+    public LivroModel criarLivro(LivroModel livroModel){
+        return livroRepository.save(livroModel);
+    }
+
+    public List<LivroModel> findAll(){
+        return livroRepository.findAll();
+    }
+
+    public void delete(Long id){
+        livroRepository.deleteById(id);
+    }
+
+    public LivroModel update(Long id, LivroModel livroModel){
+        LivroModel livro = livroRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Livro não encontrado"));
+
+        livro.setTituloLivro(livroModel.getTituloLivro());
+        livro.setAutor(livroModel.getAutor());
+        livro.setAnoPublicacao(livroModel.getAnoPublicacao());
+
+        return livroRepository.save(livro);
+    }
+}
